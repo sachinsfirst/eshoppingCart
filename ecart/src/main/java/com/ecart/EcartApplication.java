4d1672ae7cfea37163753800dedb9386d07fa24e
sachinsfirst <sachinsfirst@mail.com>
2020-02-04 21:19:42 +0530

    initial commit

---
+++ ecart/src/main/java/com/ecart/EcartApplication.java
@@ -0,0 +1,17 @@
+package com.ecart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sachins
+ *
+ */
+@SpringBootApplication
+public class EcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcartApplication.class, args);
+	}
+
+}
